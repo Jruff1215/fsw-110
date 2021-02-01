6e12b993630e755a3691825c1dec15e7cafea17c
Jruff1215 <Jamie.Ruffin@bryanuniversity.edu>
2021-01-31 22:21:49 -0500

    appending

--- week1/selecting_elements/index.js
+++ week1/selecting_elements/index.js
@@ -1,2 +1,6 @@
 var elements = document.getElementById("elements").textContent = "Selecting Elements";
 console.log(elements)
+
+var person = {name: "Joe"}
+person.name = "Jim"
+console.log(person)
---
+++ week2/index.js
@@ -0,0 +1,11 @@
+var welcome  = document.createElement("h1")
+welcome.textContent = "Welcome to my JS site"
+var text1 = document.getElementById("header")
+text1.append(welcome)
+console.log(text1.textContent)
+var p1 = document.createElement("p")
+p1.textContent = "All of this was created with Javascript"
+document.body.append(p1)
+var list = document.createElement("li")
+list.textContent = "three"
+list1.append(list)
