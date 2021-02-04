c002879770740d224db5f3a7fa4ad41c233ce37e
Jruff1215 <Jamie.Ruffin@bryanuniversity.edu>
2021-02-03 22:20:41 -0500

    fix commit

--- week2/index.js
+++ week2/index.js
@@ -1,11 +1,15 @@
-var welcome  = document.createElement("h1")
-welcome.textContent = "Welcome to my JS site"
-var text1 = document.getElementById("header")
-text1.append(welcome)
-console.log(text1.textContent)
+var h = document.createElement("h1")
+h.textContent = "Welcome to my JS Site"
+document.body.append(h)
 var p1 = document.createElement("p")
 p1.textContent = "All of this was created with Javascript"
 document.body.append(p1)
 var list = document.createElement("li")
-list.textContent = "three"
+list.textContent = "One"
 document.body.append(list)
+var list2 = document.createElement("li")
+list2.textContent = "Two"
+document.body.append(list2)
+var list3 = document.createElement("li")
+list3.textContent = "Three"
+document.body.append(list3)
